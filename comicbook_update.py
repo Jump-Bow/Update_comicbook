c2fd229b04c498b55ab4a55c083bb94c3fb097f2
Jump-Bow <asdpoi0825@gmail.com>
2022-01-02 22:02:24 +0800

    有時候網頁無法開啟會有問題，造成程式中斷，新增防呆，出問題時重新載入讀取

--- comicbook_update.py
+++ comicbook_update.py
@@ -3,7 +3,6 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-
 def main():
     #紀錄有更新再進行記錄更新d
     update_flag = False
@@ -21,9 +20,12 @@ def main():
     combooksave =[]
     # 把每一行的漫畫都抓出來並分開
     for i in content_list:
+        newcomic= "error"
         combook = i.split(',')
-        #進行資料更新
-        newcomic = send_update(combook[0], combook[1], combook[2],ntf_Group)
+        #網頁開起錯誤後進行重新載入動作
+        while newcomic=="error":
+            #進行資料更新
+            newcomic = send_update(combook[0], combook[1], combook[2],ntf_Group)
         if newcomic != "None" :
             combook[2] = newcomic 
             update_flag = True
@@ -39,10 +41,7 @@ def main():
     print("END")
 
 #漫畫名稱 , 漫畫網路位置 , 最新更新的集數
-
-
 def send_update(bookname, bookrul, bookold,ntf_Group):
-    
     # #抓取漫畫網頁
     # response = requests.get(bookrul)
     # soup = BeautifulSoup(response.text, "html.parser")
@@ -52,39 +51,51 @@ def send_update(bookname, bookrul, bookold,ntf_Group):
 
     # 神秘換電腦後 edge 無法用換 火狐
     selm = webdriver.Firefox()
+    #新增例外輸錯處理
+    try:
     #selm = webdriver.Edge(executable_path=r'D:\python\update_book\Update_comicbook\msedgedriver.exe')
     #selm = webdriver.Edge('./msedgedriver')
     #selm = webdriver.Chrome('./chromedriver')
-    selm.get (bookrul)
-    # booknew=selm.find_elements_by_class_name("fed-part-eone")[26] if  selm.find_elements_by_class_name("fed-part-eone")[26].text!='排序：正序 展开' else selm.find_elements_by_class_name("fed-part-eone")[27]
+        selm.get (bookrul)
+        # booknew=selm.find_elements_by_class_name("fed-part-eone")[26] if  selm.find_elements_by_class_name("fed-part-eone")[26].text!='排序：正序 展开' else selm.find_elements_by_class_name("fed-part-eone")[27]
 
-    booknew=selm.find_elements_by_class_name("fed-rims-info")[1]
+        booknew=selm.find_elements_by_class_name("fed-rims-info")[1]
 
-    #沒有新集數，跳出
-    # if bookold == booknew:
-    if bookold == booknew.text:
-        return "None"
-    #抓取最新集數html位置
-    #comichref = soup.findAll('a', attrs={"class": "fed-btns-info fed-rims-info fed-part-eone", "href": re.compile(".html")}, limit=1)[0].get('href')
-    comichref=booknew.get_attribute('href')
-    #將發送的 notify key 填入 發送
-    for group in ntf_Group:
-        headers = {
-            "Authorization": "Bearer " + group.split(',')[0],
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-        # params = {"message": bookname + ","+booknew + "  "+"https://www.cocomanhua.com"+comichref}
-        params = {"message": bookname + ","+booknew.text + "  "+str(comichref)}
-        r = requests.post("https://notify-api.line.me/api/notify",
-                        headers=headers, params=params)
-        print(r.status_code)  # 200
+        #沒有新集數，跳出
+        # if bookold == booknew:
+        if bookold == booknew.text:
+            return "None"
+        #抓取最新集數html位置
+        #comichref = soup.findAll('a', attrs={"class": "fed-btns-info fed-rims-info fed-part-eone", "href": re.compile(".html")}, limit=1)[0].get('href')
+        comichref=booknew.get_attribute('href')
+        #將發送的 notify key 填入 發送
+        for group in ntf_Group:
+            headers = {
+                "Authorization": "Bearer " + group.split(',')[0],
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+            # params = {"message": bookname + ","+booknew + "  "+"https://www.cocomanhua.com"+comichref}
+            params = {"message": bookname + ","+booknew.text + "  "+str(comichref)}
+            r = requests.post("https://notify-api.line.me/api/notify",
+                            headers=headers, params=params)
+            print(r.status_code)  # 200
 
-    # 回傳新集數
-    rtnmsg =  booknew.text
+        # 回傳新集數
+        rtnmsg =  booknew.text
 
-    # 關閉 browser
-    selm.quit()
+        # 關閉 browser
+        selm.quit()
+        
+    except Exception as e:
+        
+        selm.quit()
+        return "error"
+
+
     return rtnmsg
+#===============  2021/10/17 grequests update
+
+
 
 
 if __name__ == '__main__':
